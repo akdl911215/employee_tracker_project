97da7c8a18ee0c89e61ef7c343ed6a041a179715
akdl911215 <akdl911215@naver.com>
2024-11-25 16:14:29 +0900

    java version

--- build.gradle.kts
+++ build.gradle.kts
@@ -40,10 +40,10 @@ tasks.test {
 }
 
 kotlin {
-    jvmToolchain(17)
+    jvmToolchain(21)
 }
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_17
-    targetCompatibility = JavaVersion.VERSION_17
+    sourceCompatibility = JavaVersion.VERSION_21
+    targetCompatibility = JavaVersion.VERSION_21
 }
