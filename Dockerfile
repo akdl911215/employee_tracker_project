e70c1fe3112cafe21593f45597b3e7bc1c1fce01
akdl911215 <akdl911215@naver.com>
2024-11-25 10:30:11 +0900

    git automatic deployment task

---
+++ Dockerfile
@@ -0,0 +1,16 @@
+FROM openjdk:17-jdk-slim
+
+WORKDIR /app
+
+COPY build/libs/your-app-name-0.0.1-SNAPSHOT.jar app.jar
+
+COPY package*.json ./
+COPY prisma ./prisma/
+
+ENV DATABASE_URL=postgresql://postgres:123456@43.203.208.39:5432/postgres
+ENV PORT=8080
+ENV HOST=43.203.208.39
+
+EXPOSE 8080
+
+ENTRYPOINT ["java", "-jar", "app.jar"]
--- build.gradle.kts
+++ build.gradle.kts
@@ -6,6 +6,12 @@ plugins {
     id("io.spring.dependency-management") version "1.1.0"
 }
 
+tasks.register<Exec>("dockerBuild") {
+    group = "docker"
+    description = "Build Docker Image"
+    commandLine("docker", "build", "-t", "your-dockerhub-username/your-app-name:latest", ".")
+}
+
 group = "org.jh"
 version = "1.0-SNAPSHOT"
 
@@ -27,12 +33,6 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
     implementation("org.springframework.boot:spring-boot-configuration-processor")
     implementation("io.github.cdimascio:dotenv-kotlin:6.4.1")
-
-    implementation("io.ktor:ktor-client-core:2.3.0")
-    implementation("io.ktor:ktor-client-cio:2.3.0") // CIO engine for HTTP
-    implementation("io.ktor:ktor-client-content-negotiation:2.3.0") // JSON support
-    implementation("io.ktor:ktor-serialization-kotlinx-json:2.3.0") // Serialization
-
     implementation("com.squareup.okhttp3:okhttp:4.11.0")
 
 }
